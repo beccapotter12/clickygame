113a2f229bd9158f8dd0436b88837594cb7b4981
Rebecca Potter <rebeccapotter@Rebeccas-Air.hsd1.ut.comcast.net>
2019-06-22 23:57:21 -0600

    header and background

---
+++ src/components/Card/index.js
@@ -0,0 +1,7 @@
+// import React from "react";
+// import "./style.css";
+
+// function Cards(props) {
+    
+// }
+//   export default Cards;
---
+++ src/components/CardBox/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./style.css";
+
+function Cards(props) {
+    return (
+        <h1 class="cardBox">This is our cards</h1>
+    );
+}
+  export default Cards;
